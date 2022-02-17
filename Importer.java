4d889ddbccad64199b95ac136637e3d4827fe945
YAMAGIRI SALEEMBASHA <95207243+yamagirisaleembasha@users.noreply.github.com>
2022-02-17 12:41:26 +0530

    cs253 oop\Examples\packages

---
+++ Importer.java
@@ -0,0 +1,10 @@
+import pack.*;
+import pack.subpack.*;
+class Importer
+{
+public static void main(String arg[])
+{
+Class1.greet();
+Class2.farewell();
+}
+}
---
+++ Testpack.java
@@ -0,0 +1,14 @@
+import p1.*;
+class Testpack
+{
+public static void main(String args[])
+{
+Lmn a =new Lmn();
+System.out.println("Creating an object of p1 package Lmn class...");
+System.out.println("Possible because of Importing");
+System.out.println("Calling method of Lmn class...");
+System.out.println("===========================");
+a.m3();
+System.out.println("===========================");
+}
+}
