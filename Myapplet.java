080de43c22363fb0d9d6fccaf3d77dd69682d00d
YAMAGIRI SALEEMBASHA <95207243+yamagirisaleembasha@users.noreply.github.com>
2022-03-10 13:34:31 +0530

    week 11 ii

--- Myapplet.java
+++ Myapplet.java
@@ -13,14 +13,14 @@ public class Myapplet extends Applet
 		setBackground(Color.red);
 		g.setFont(f1);
 		g.drawString("Welcome SALEEMBASHA",30,40); 
-		g.drawLine(20,30,20,300);  
-		g.drawRect(70,100,30,30);
-		g.fillRect(170,100,30,30);  
-		g.drawOval(70,200,20,30);  
+		g.drawLine(20,50,20,150);  
+		g.drawRect(30,50,70,30);
+		g.fillRect(40,100,30,30);  
+		g.drawOval(120,50,20,30);  
 		g.setColor(Color.pink);
-		g.fillOval(170,200,30,20);  
-		g.drawArc(90,150,30,30,30,270);  
-		g.fillArc(270,150,30,30,0,180);
+		g.fillOval(130,100,30,20);  
+		g.drawArc(40,150,30,30,30,270);  
+		g.fillArc(120,150,30,30,0,180);
 	}  
 }
 /*<applet code="Myapplet.class" width="300" height="200">
